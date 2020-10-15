2fcc01e44c4181a5acd8a4babbe76ae5d09aeb42
pujiermanto <pujiermanto@gmail.com>
2020-10-15 09:15:53 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+$url = "https://bla-bla.com";
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// get headers too with this line
+curl_setopt($ch, CURLOPT_HEADER, 1);
+$result = curl_exec($ch);
+// get cookie
+// multi-cookie variant contributed by @Combuster in comments
+preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $result, $matches);
+$cookies = array();
+foreach($matches[1] as $item) {
+    parse_str($item, $cookie);
+    $cookies = array_merge($cookies, $cookie);
+}
+var_dump($cookies);
